90d4a25b786bad1f841cdb985abc4340e0d46c93
Rhunnan <rhunnandwight.liao.21@usjr.edu.ph>
2024-03-14 21:19:27 +0800

    added viewsets for the models and serializers

--- kritikApp/views.py
+++ kritikApp/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from . import models
+from . import serializers
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = models.User.objects.all()
+    serializer_class = serializers.UserSerializer
+
+class EstablishmentViewSet(viewsets.ModelViewSet):
+    queryset = models.Establishment.objects.all()
+    serializer_class = serializers.EstablishmentSerializer
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = models.Review.objects.all()
+    serializer_class = serializers.ReviewSerializer
+    
